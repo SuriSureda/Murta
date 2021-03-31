b93e644c028442a21cbcc7d801e8a4c4838fe3cb
SuriSureda <suredaps4@gmail.com>
2021-03-31 20:40:43 +0200

    changed colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,19 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const red = '#F75E68';
+const gray = '#707070';
 
 export default {
   light: {
     text: '#000',
     background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    tint: red,
+    tabIconDefault: gray,
+    tabIconSelected: red,
   },
   dark: {
     text: '#fff',
     background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+    tint: red,
+    tabIconDefault: gray,
+    tabIconSelected: red,
   },
 };
