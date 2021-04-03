ec7a31db304b75723c3e721da146b586780649ba
SuriSureda <suredaps4@gmail.com>
2021-04-03 18:15:38 +0200

    Main and Profile Screen added

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -1,14 +1,15 @@
 import { Ionicons } from '@expo/vector-icons';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import * as React from 'react';
-import { View, ViewProps } from '../components/Themed';
+import { View } from '../components/Themed';
 import { StyleSheet } from 'react-native';
 
 import Colors from '../constants/Colors';
 import useColorScheme from '../hooks/useColorScheme';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { BottomTabParamList} from '../types';
+import { BottomTabParamList, MainTabParamList, ProfileTabParamList} from '../types';
+import MainScreen from '../screens/MainScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 
 const BottomTab = createBottomTabNavigator<BottomTabParamList>();
 
@@ -24,25 +25,25 @@ export default function BottomTabNavigator() {
 
   return (
     <BottomTab.Navigator
-      initialRouteName="TabOne"
+      initialRouteName="MainTab"
       tabBarOptions={{ activeTintColor: Colors[colorScheme].tint, showLabel :false, style : styles.container}}>
       <BottomTab.Screen
-        name="TabOne"
-        component={TabOneScreen}
+        name="ProfileTab"
+        component={ProfileTabNavigator}
         options={{
           tabBarIcon: ({ color, focused }) => <TabBarIcon name="person" color={color} focused={focused}/>,
         }}
       />
       <BottomTab.Screen
-        name="TabTwo"
-        component={TabTwoScreen}
+        name="MainTab"
+        component={MainTabNavigator}
         options={{
           tabBarIcon: ({ color, focused }) => <TabBarIcon name="home" color={color} focused={focused} />,
         }}
       />
       <BottomTab.Screen
-        name="TabThree"
-        component={TabTwoScreen}
+        name="RegisterTab"
+        component={MainTabNavigator}
         options={{
           tabBarIcon: ({ color, focused }) => <TabBarIcon name="calendar" color={color} focused={focused}/>,
         }}
@@ -51,8 +52,6 @@ export default function BottomTabNavigator() {
   );
 }
 
-// You can explore the built-in icon families and icons on the web at:
-// https://icons.expo.fyi/
 function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']; color: string; focused : boolean }) {
 
   const size : number = props.focused ? 32 : 25
@@ -67,7 +66,7 @@ function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']
     line : {
       backgroundColor : props.focused ? Colors[colorScheme].tabIconSelected : Colors[colorScheme].background, 
       height : "10%",
-      marginBottom : "5%",
+      marginBottom : "20%",
       width : "100%",
       borderRadius: 10000
     }
@@ -80,5 +79,33 @@ function TabBarIcon(props: { name: React.ComponentProps<typeof Ionicons>['name']
     </View>
     
   )
+}
+
+const ProfileTabStack = createStackNavigator<ProfileTabParamList>();
+
+function ProfileTabNavigator() {
+  return (
+    <ProfileTabStack.Navigator>
+      <ProfileTabStack.Screen
+        name="ProfileScreen"
+        component={ProfileScreen}
+        options={{ headerShown : false }}
+      />
+    </ProfileTabStack.Navigator>
+  );
+}
+
+const MainTabStack = createStackNavigator<MainTabParamList>();
+
+function MainTabNavigator() {
+  return (
+    <MainTabStack.Navigator>
+      <MainTabStack.Screen
+        name="MainScreen"
+        component={MainScreen}
+        options={{  headerShown : false }}
+      />
+    </MainTabStack.Navigator>
+  );
 }
 
