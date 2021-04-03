3e7523e1dc696546030d847fea5b078c8ab61014
SuriSureda <suredaps4@gmail.com>
2021-04-03 18:14:52 +0200

    added colors

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,7 +1,16 @@
 const red = '#F75E68';
 const gray = '#707070';
+const softgray = '#A1A1A1';
+const orange = '#FFC766';
+const white = '#FFFFFF';
+
 
 export default {
+  red : red,
+  gray : gray,
+  softgray : softgray,
+  orange : orange,
+  white : white,
   light: {
     text: '#000',
     background: '#fff',
